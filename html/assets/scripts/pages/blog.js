1d7c660d882f3412012815495628cb67ae7b028f
Tijana Basic <t.basic@vegaitsourcing.rs>
2015-08-13 14:58:02 +0200

    updated blog page

--- html/assets/scripts/pages/blog.js
+++ html/assets/scripts/pages/blog.js
@@ -3,3 +3,15 @@ $(document).ready(function () {
 	$('select').sSelect();
 
 });
+
+$(window).load(function () {
+
+	var $container = $('.blog-inner-wrap');
+		$container.packery({
+			transitionDuration: '0.5s',
+		  	itemSelector: '.item',
+			stamp: '.stamp',
+			isResizeBound: true
+		});
+
+});
