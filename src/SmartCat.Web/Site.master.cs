f0a56a3e5a15690b28c78ba565ad2ab7e5ac811c
mgobec <matija0204@gmail.com>
2015-08-17 11:06:27 +0200

    Added smtp client and mail sending

--- src/SmartCat.Web/Site.master.cs
+++ src/SmartCat.Web/Site.master.cs
@@ -1,13 +1,30 @@
 ﻿namespace SmartCat.Web
 {
     using System;
+    using System.Net.Mail;
     using System.Web.UI;
 
     public class Site : MasterPage
     {
+        //Smpt server  
+        public const string GmailServer = "aspmx.l.google.com";
+        //Connecting port  
+        public const int Port = 25;
+
         protected void OnContactUsClick(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            using (var client = new SmtpClient(GmailServer, Port))
+            {
+                //Request.Form[""]
+
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress("matija0204@gmail.com");
+                mail.To.Add("info@smartcat.io");
+                mail.Subject = "Contact us";
+                mail.Body = "I'm contacting you from your smartcat.io web application";
+
+                //client.Send(mail);
+            }
         }
     }
 }
