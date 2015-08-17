2e636d132e1ee2c66407c0e90f6c372796aac255
mgobec <matija0204@gmail.com>
2015-08-17 15:47:20 +0200

    Updated mail sending and connected to contacts page

--- src/SmartCat.Web/Site.master.cs
+++ src/SmartCat.Web/Site.master.cs
@@ -11,7 +11,7 @@
         //Connecting port  
         public const int Port = 25;
 
-        protected void OnContactUsClick(object sender, EventArgs e)
+        public void OnContactUsClick(object sender, EventArgs e)
         {
             // Send email to info using gmails anonymous email sending server
             // This should be updated to IP based smtp relay service https://support.google.com/a/answer/176600?hl=en
@@ -22,14 +22,19 @@
                 {
                     var senderName = Request.Form["SenderName"];
                     var senderEmail = Request.Form["SenderEmail"];
+                    var senderContact = Request.Form["SenderContact"];
                     var subject = Request.Form["Subject"];
                     var message = Request.Form["Message"];
 
                     var mail = new MailMessage();
                     mail.From = new MailAddress(senderEmail);
                     mail.To.Add("info@smartcat.io");
-                    mail.Subject = senderName + " | " + subject;
-                    mail.Body = message;
+                    mail.Subject = subject;
+                    mail.Body = senderName;
+                    mail.Body += Environment.NewLine;
+                    mail.Body += senderContact;
+                    mail.Body += Environment.NewLine;
+                    mail.Body += message;
 
                     client.Send(mail);
                 }
